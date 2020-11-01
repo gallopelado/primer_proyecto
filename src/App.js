448c0006c6a7f612a50e10d6dc98d0af08920480
Juan <juanftp100@gmail.com>
2020-10-31 21:44:43 -0300

    add: probando banner

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import {Fragment} from 'react';
 import "./styles/styles.scss"
 
 function App() {
   return (
-    <Fragment>
-      <div>
-        <h1>Hola mundo desde App.js</h1>
-        <p>Saludando desde mi componente</p>
+    <div className="main-banner img-container" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" src="https://images.pexels.com/photos/265614/pexels-photo-265614.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" alt="laimagen" />
+            <div className="main-banner__data s-center">
+              <p className="t2 s-mb-0">Título del banner</p>
+              <p> Subtítulo del banner</p>
+              <a href="/entrando" className="button">Botón del banner</a>
+            </div>
+        </div>
       </div>
-      <div>
-        <h1>Hola soy otro div</h1>
-        <p>Saludando desde mi componente</p>
-      </div>
-      <div>
-        <img src="https://ux.ed.team/img/curso-portada.png" alt="prueba" />
-      </div>
-    </Fragment>
+    </div>
   );
 }
 
