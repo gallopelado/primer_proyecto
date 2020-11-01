c72d81e134107b22ca7782e1e51388ab275e52c8
Juan <juanftp100@gmail.com>
2020-10-31 22:08:32 -0300

    add: jugando con nuevos componentes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./styles/styles.scss"
+import Curso from './Curso'
 
 function App() {
   return (
-    <div className="main-banner img-container" id="main-banner">
+    <>
+    <div className="main-banner img-container l-section" id="main-banner">
       <div className="ed-grid lg-grid-6">
         <div className="lg-cols-4 lg-x-2">
           <img className="main-banner__img" src="https://images.pexels.com/photos/265614/pexels-photo-265614.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" alt="laimagen" />
             <div className="main-banner__data s-center">
-              <p className="t2 s-mb-0">Título del banner</p>
-              <p> Subtítulo del banner</p>
-              <a href="/entrando" className="button">Botón del banner</a>
+              <p className="t2 s-mb-0">Bienvenido</p>
+              <p> Tu futuro esta cerca</p>
+              <a href="/entrando" className="button">Ingresar</a>
             </div>
         </div>
       </div>
     </div>
+
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+
+    </>
   );
 }
 
