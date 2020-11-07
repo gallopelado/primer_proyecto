2ea0696d8dc4293f1213eb0c3e3f6d158151337c
Juan <juanftp100@gmail.com>
2020-11-07 11:33:23 -0300

    cambiando estado del componente

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -4,11 +4,39 @@ class Formulario extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            nombre: '',
+            correo: ''
+        }
     }
+
+    // this.setState({})
 
     render() {
         return (
-            <div className="ed-grid"><h1>Formulario</h1></div>
+            <div className="ed-grid">
+                <h1>Formulario</h1>
+                <form>
+                    <div className="ed-grid m-grid-2">
+                        <div className="form__item">
+                            <label htmlFor="">Nombre Completo</label>
+                            <input type="text" name="" onChange={ (e) => this.setState({
+                                 nombre: e.target.value
+                            }) } />
+                        </div>
+                        <div className="form__item">
+                            <label htmlFor="">Correo Electronico</label>
+                            <input type="email" onChange={ (e) => this.setState({
+                                correo: e.target.value
+                            }) } />
+                        </div>
+                    </div>
+                </form>
+                <div>
+                    <h2>{ `Hola ${this.state.nombre}` }</h2>
+                    <span>{`Tu correo es: ${this.state.correo}`}</span>
+                </div>
+            </div>
         )
     }
 }
