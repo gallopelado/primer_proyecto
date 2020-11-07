ba7913c2b102f05c76aaac00ae3a83b23270b340
Juan <juanftp100@gmail.com>
2020-11-07 12:59:27 -0300

    usando ciclos de vida

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Banner />
-      <Formulario />
+      <Formulario name="El Formulario Salvaje" />
     </>
   );
 }
--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -6,11 +6,13 @@ class Formulario extends Component {
         super(props)
         this.state = {
             nombre: '',
-            correo: ''
+            correo: '',
+            fecha: new Date()
         }
         //Enlazar
         this.cambiarNombre = this.cambiarNombre.bind(this)
         this.cambiarCorreo = this.cambiarCorreo.bind(this)
+        this.cambiarFecha = this.cambiarFecha.bind(this)
     }
 
     cambiarNombre(e) {
@@ -24,13 +26,20 @@ class Formulario extends Component {
             correo: e.target.value
         })
     }
+
+    cambiarFecha() {
+        this.setState({
+            fecha: new Date()
+        })
+    }
     // this.setState({})
 
     render() {
         return (
             <div className="ed-grid">
-                <h1>Formulario</h1>
-                <form>
+                <h1>Formulario: {this.props.name}</h1>
+                <h2>Fecha: {Math.ceil(this.state.fecha/1000)}</h2>
+                <form id="elemento">
                     <div className="ed-grid m-grid-2">
                         <div className="form__item">
                             <label htmlFor="">Nombre Completo</label>
@@ -49,6 +58,27 @@ class Formulario extends Component {
             </div>
         )
     }
+
+    componentDidMount() {
+        const elemento = document.getElementById('elemento');
+        console.log(elemento);
+        this.intervaloFecha = setInterval( () => {
+            this.cambiarFecha()
+            console.log(new Date());
+        }, 1000)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        /* console.log('Props: ', prevProps);
+        console.log('State: ', prevState);
+        console.log('------------------------'); */
+    }
+
+    componentWillUnmount() {
+        // cuando se desmonta la app, apagamos el interval
+        clearInterval(this.intervaloFecha)
+    }
+
 }
 
 export default Formulario;
